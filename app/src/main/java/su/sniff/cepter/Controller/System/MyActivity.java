a5b464fb09e160d82df9617b0b3feff364d17768
Adeline <adeline.galasso@epitech.eu>
2017-09-01 13:28:20 +0200

    Style + AppCompatActivity + drawable for menu

--- app/src/main/java/su/sniff/cepter/Controller/System/MyActivity.java
+++ app/src/main/java/su/sniff/cepter/Controller/System/MyActivity.java
@@ -1,11 +1,11 @@
 package su.sniff.cepter.Controller.System;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 import su.sniff.cepter.Controller.System.Wrapper.ArpSpoof;
 
 
-public class                    MyActivity extends Activity {
+public class                    MyActivity extends AppCompatActivity {
     protected String            TAG = this.getClass().getName();
     protected MyActivity        mInstance = this;
 
