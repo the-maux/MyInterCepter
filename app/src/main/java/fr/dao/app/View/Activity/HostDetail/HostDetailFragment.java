8c19e9d88ae6a577587328cd46785d5272bb1da5
MAXIME RENAUD <maxime.renaud@ext.leroymerlin.fr>
2018-03-12 17:37:29 +0100

    HostDiscovery(UpnP): Improvements

--- app/src/main/java/fr/dao/app/View/Activity/HostDetail/HostDetailFragment.java
+++ app/src/main/java/fr/dao/app/View/Activity/HostDetail/HostDetailFragment.java
@@ -1,7 +1,6 @@
 package fr.dao.app.View.Activity.HostDetail;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
@@ -44,8 +43,7 @@ public class                    HostDetailFragment extends MyFragment {
     public void                 init() {
         Bundle args = getArguments();
         if (args == null || args.getString("macAddress") == null) {
-            startActivity(new Intent(getActivity(), HostDiscoveryActivity.class));
-            return;
+            mActivity.showSnackbar("Error in focus device (no MAC) in bundle");
         } else {
             mFocusedHost = DBHost.getDevicesFromMAC(args.getString("macAddress"));
             mRV.setAdapter(adapter);
@@ -64,37 +62,78 @@ public class                    HostDetailFragment extends MyFragment {
 
     private ArrayList<String[]> buildInfoArray() {
         ArrayList<String[]> arrayList = new ArrayList<>();
-        String[] title1 = {"Name", mFocusedHost.name};
-        String[] title2 = {"IP Address", mFocusedHost.ip};
-        String[] title3 = {"MAC Address", mFocusedHost.mac};
-        String[] title4 = {"Device Type", mFocusedHost.osType.name().toUpperCase()};
-        String[] title5 = {"MAC Vendor", mFocusedHost.vendor};
-        String[] title6 = {"NetBIOS Domain", mFocusedHost.NetBIOS_Domain};
-        String[] title7 = {"NetBIOS Name", mFocusedHost.NetBIOS_Name};
-        String[] title8 = {"NetBIOS Role", mFocusedHost.NetBIOS_Role};
-        String[] title9 = {"First seen", mFocusedHost.getDateString()};
-        String[] title10 = {"Brand and Model", mFocusedHost.Brand_and_Model};
-        String[] title11 = {"Bonjour Name", mFocusedHost.Bonjour_Name};
-        String[] title12 = {"Bonjour Services", mFocusedHost.Bonjour_Services};
-        String[] title13 = {"UPnP Name", mFocusedHost.UPnP_Name};
-        String[] title14 = {"UPnP Device", mFocusedHost.UPnP_Device};
-        String[] title15 = {"UPnP Services", mFocusedHost.UPnP_Services};
-        arrayList.add(title1);
-        arrayList.add(title2);
-        arrayList.add(title3);
-        arrayList.add(title4);
-        arrayList.add(title5);
-        arrayList.add(title6);
-        arrayList.add(title7);
-        arrayList.add(title8);
-        arrayList.add(title9);
-        arrayList.add(title10);
-        arrayList.add(title11);
-        arrayList.add(title12);
-        arrayList.add(title13);
-        arrayList.add(title14);
-        arrayList.add(title15);
+        buildBasicInfos(arrayList);
+        buildUpnP(arrayList);
+        buildNetBIOS(arrayList);
+        buildBonjour(arrayList);
         return arrayList;
+    }
+
+    private void                buildBasicInfos(ArrayList<String[]> arrayList) {
+        String[] title1 = {"Name", mFocusedHost.name};
+        arrayList.add(title1);
+        String[] title2 = {"IP Address", mFocusedHost.ip};
+        arrayList.add(title2);
+        String[] title3 = {"MAC Address", mFocusedHost.mac};
+        arrayList.add(title3);
+        if (!mFocusedHost.deviceType.contains("Unknown")) {
+            String[] title4 = {"Device Type", mFocusedHost.osType.name().toUpperCase()};
+            arrayList.add(title4);
+        }
+        String[] title5 = {"MAC Vendor", mFocusedHost.vendor};
+        arrayList.add(title5);
+        String[] title9 = {"First seen", mFocusedHost.getDateString()};
+        arrayList.add(title9);
+        if (!mFocusedHost.Brand_and_Model.contains("Unknown")) {
+            String[] title10 = {"Brand and Model", mFocusedHost.Brand_and_Model};
+            arrayList.add(title10);
+        }
+
+    }
+
+    private void                buildBonjour(ArrayList<String[]> arrayList) {
+        if (mFocusedHost.Bonjour_Name.contains("Unknown") && mFocusedHost.Bonjour_Services.contains("Unknown")) {
+            String[] title11 = {"Bonjour Service", "No configuration detected"};
+            arrayList.add(title11);
+        } else {
+            String[] title11 = {"Bonjour Name", mFocusedHost.Bonjour_Name};
+            String[] title12 = {"Bonjour Services", mFocusedHost.Bonjour_Services};
+            arrayList.add(title11);
+            arrayList.add(title12);
+        }
+    }
+
+    private void                buildNetBIOS(ArrayList<String[]> arrayList) {
+        if (mFocusedHost.NetBIOS_Domain.contains("Unknown") &&
+                mFocusedHost.NetBIOS_Domain.contains("Unknown") &&
+                mFocusedHost.NetBIOS_Domain.contains("Unknown")) {
+            String[] title6 = {"NetBIOS", "No configuration detected"};
+            arrayList.add(title6);
+        } else {
+            String[] title6 = {"NetBIOS Domain", mFocusedHost.NetBIOS_Domain};
+            String[] title7 = {"NetBIOS Name", mFocusedHost.NetBIOS_Name};
+            String[] title8 = {"NetBIOS Role", mFocusedHost.NetBIOS_Role};
+            arrayList.add(title6);
+            arrayList.add(title7);
+            arrayList.add(title8);
+        }
+    }
+
+    private void                buildUpnP(ArrayList<String[]> arrayList) {
+        if (mFocusedHost.UPnP_Name.contains("Unknown") &&
+                mFocusedHost.UPnP_Device.contains("Unknown") &&
+                mFocusedHost.UPnP_Services.contains("Unknown")) {
+            String[] title6 = {"UPnP Services", "No configuration detected"};
+            arrayList.add(title6);
+        } else {
+            String[] title13 = {"UPnP Name", mFocusedHost.UPnP_Name};
+            String[] title14 = {"UPnP Device", mFocusedHost.UPnP_Device};
+            String[] title15 = {"UPnP Services", mFocusedHost.UPnP_Services};
+            arrayList.add(title13);
+            arrayList.add(title14);
+            arrayList.add(title15);
+        }
+
     }
 
 }
